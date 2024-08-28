d85b8ded3be86fb372b131bf886e859c99143ef7
StepanPotiienko <kotnexjieb@protonmail.com>
2024-08-28 15:47:11 +0300

    Added a function that prints out array with any type, changed array type from i8 to i32

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use std::fmt::Display;
+
 fn main()  {
     // No type annotation, no fixed size. Just pure madness.
     let tuple = ("hello", "2", '1', true, 1);
     
     // Type and size are required. 
-    let array: [i8; 5] = [1, 2, 3, 4, 5];
+    // Changed from i8 to i32 because some numbers were greater than i8 max range.
+    let array: [i32; 5] = [1, 23, 133, 4512, 56129312];
 
     // How different things can be.
     println!("{}", tuple.0);
@@ -11,8 +14,11 @@ fn main()  {
 
     // Slicing array like a cake.
     println!("{:?}", &array[1..4]);
+    print_array(&array);
+}
 
-    // Every Python developer worst nightmare.
+// We use <T: Display> to ensure that we accept arrays with any type, not just one particular.
+fn print_array<T: Display>(array: &[T]) {
     for i in 0..array.len() {
         match array.get(i) {
             Some(val) => println!("{} : {}", i, val),
@@ -20,3 +26,4 @@ fn main()  {
         }
     }
 }
+
