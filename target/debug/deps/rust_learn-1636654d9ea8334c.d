6f7d57ca155fac9afd3143233ca62e9223fde286
StepanPotiienko <kotnexjieb@protonmail.com>
2024-08-28 14:02:26 +0300

    master to main merge

---
+++ target/debug/deps/rust_learn-1636654d9ea8334c.d
@@ -0,0 +1,5 @@
+/home/stepan-fedora/Desktop/rust_learn/rust_learn/target/debug/deps/rust_learn-1636654d9ea8334c: src/main.rs
+
+/home/stepan-fedora/Desktop/rust_learn/rust_learn/target/debug/deps/rust_learn-1636654d9ea8334c.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_learn.d
@@ -0,0 +1,1 @@
+/home/stepan-fedora/Desktop/rust_learn/rust_learn/target/debug/rust_learn: /home/stepan-fedora/Desktop/rust_learn/rust_learn/src/main.rs
