c00f8218804115663bf8a95c1f857baacc35455f
StepanPotiienko <138608284+StepanPotiienko@users.noreply.github.com>
2024-08-28 14:04:54 +0300

    Delete target directory

--- target/debug/deps/rust_learn-1636654d9ea8334c.d
+++
@@ -1,5 +0,0 @@
-/home/stepan-fedora/Desktop/rust_learn/rust_learn/target/debug/deps/rust_learn-1636654d9ea8334c: src/main.rs
-
-/home/stepan-fedora/Desktop/rust_learn/rust_learn/target/debug/deps/rust_learn-1636654d9ea8334c.d: src/main.rs
-
-src/main.rs:
--- target/debug/rust_learn.d
+++
@@ -1,1 +0,0 @@
-/home/stepan-fedora/Desktop/rust_learn/rust_learn/target/debug/rust_learn: /home/stepan-fedora/Desktop/rust_learn/rust_learn/src/main.rs
